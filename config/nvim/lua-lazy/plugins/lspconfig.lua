7cf291da0810e8b99f22cd268b7da8e47c97fe25
Michael McGloin <mcgloinms@gmail.com>
2023-03-20 10:20:57 -0400

    fix(nvim): Fix formatting of lspconfig files

--- config/nvim/lua-lazy/plugins/lspconfig.lua
+++ config/nvim/lua-lazy/plugins/lspconfig.lua
@@ -1,4 +1,4 @@
 return {
   'neovim/nvim-lspconfig',
-
+  'jose-elias-alvarez/null-ls.nvim',
 }
--- config/nvim/lua-lazy/plugins/mason.lua
+++ config/nvim/lua-lazy/plugins/mason.lua
@@ -1,7 +1,11 @@
 return {
-  'williamboman/mason-lspconfig.nvim',
-  opts = {
-    automatic_installation = true,
-    auto_update = true,
+  'williamboman/mason.nvim',
+  {
+    'williamboman/mason-lspconfig.nvim',
+    opts = {
+      automatic_installation = true,
+      auto_update = true,
+    },
   },
+  'jayp0521/mason-null-ls.nvim'
 }
